5c34f6c50105c90a31f3a49c1d02dcbc44f7fe24
Tim <timweissenfels7@gmail.com>
2020-02-02 01:47:11 +0100

    Added Action as Member to Support expressions class

--- single_statement.cpp
+++ single_statement.cpp
@@ -5,7 +5,7 @@
 #include "single_statement.h"
 
 namespace grfc {
-    single_statement::single_statement(const bool _is_inverted, const grfc::identifier _literal_and_num, const grfc::conn _type) : is_inverted(_is_inverted), type(_type), literal_and_num(_literal_and_num) { }
+    single_statement::single_statement(const bool _is_inverted, const grfc::identifier _literal_and_num, const grfc::conn _type, const grfc::action _act) : is_inverted(_is_inverted), type(_type), act(_act), literal_and_num(_literal_and_num) { }
 
     char single_statement::literal() const {
         return literal_and_num.literal;
@@ -17,6 +17,10 @@ namespace grfc {
 
     grfc::conn single_statement::conn() const {
         return type;
+    }
+
+    grfc::action single_statement::action() const {
+        return act;
     }
 
     std::string single_statement::full_name() const {
