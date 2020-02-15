83f2c4bce1ceec1a314e171e057c3388e30cdd5d
Tim <timweissenfels7@gmail.com>
2020-02-15 03:22:06 +0100

    Rearranged most of user input to new namespace inside user_input.h + add new json_type_traits

--- grfctofup.cpp
+++ grfctofup.cpp
@@ -16,6 +16,7 @@
 #include "node.h"
 #include "grafcet.h"
 #include "grafcet_timer.h"
+#include "user_input.h"
 
 #define _AUTO_DEBUGING_ 1
 //#define _USER_TESTING_ 1
@@ -25,15 +26,15 @@ JSONCONS_ENUM_TRAITS_DECL(fup::bb, fc1, fc2, fc3)
 //GRAFCET CONN ENUM
 JSONCONS_ENUM_TRAITS_DECL(grfc::conn, non, dot, plus)
 //GRAFCET ACTION ENUM
-JSONCONS_ENUM_TRAITS_DECL(grfc::action, set, unset, rising_edge, falling_edge)
+JSONCONS_ENUM_TRAITS_DECL(grfc::action, set, unset, timer, rising_edge, falling_edge)
 //GRAFCET IDENTIFIER STRUCT
 JSONCONS_ALL_MEMBER_TRAITS_DECL(grfc::identifier, literal, number)
 //GRAFCET SINGLE_STATEMENT CLASS
-JSONCONS_ALL_MEMBER_TRAITS_DECL(grfc::single_statement, is_inverted, type, act, literal_and_num)
+JSONCONS_ALL_MEMBER_TRAITS_DECL(grfc::single_statement, is_inverted, type, act, literal_and_num,timer)
 //GRAFCET TRANSITION CLASS
-JSONCONS_ALL_MEMBER_TRAITS_DECL(grfc::transition, statements, timer,is_inverted)
+JSONCONS_ALL_MEMBER_TRAITS_DECL(grfc::transition, statements,is_inverted)
 //GRAFCET EXPRESSION CLASS
-JSONCONS_ALL_MEMBER_TRAITS_DECL(grfc::expression, count_blocks, act_with_literal, timer)
+JSONCONS_ALL_MEMBER_TRAITS_DECL(grfc::expression, count_blocks, action_block)
 //GRAFCET NODE CLASS
 JSONCONS_ALL_MEMBER_TRAITS_DECL(grfc::node, step, next_step, is_initial, trans, expr, connected_to_initial)
 //GRAFCET GRAFCET CLASS
@@ -41,14 +42,9 @@ JSONCONS_ALL_MEMBER_TRAITS_DECL(grfc::grafcet, count, building_block, nodes)
 //GRAFCET GRAFCET_TIMER CLASS
 JSONCONS_ALL_MEMBER_TRAITS_DECL(grfc::grafcet_timer, timestr, time, ident)
 
-void ReplaceStringInPlace(std::string& subject, const std::string& search,
-	const std::string& replace) {
-	size_t pos = 0;
-	while ((pos = subject.find(search, pos)) != std::string::npos) {
-		subject.replace(pos, search.length(), replace);
-		pos += replace.length();
-	}
-}
+//For Shared Pointer inside single_statement
+JSONCONS_POLYMORPHIC_TRAITS_DECL(grfc::identifier, grfc::identifier)
+JSONCONS_POLYMORPHIC_TRAITS_DECL(grfc::grafcet_timer, grfc::grafcet_timer)
 
 void grfc::grafcet::write_as_json_tofile(std::string name) {
 	jsoncons::json j;
@@ -71,64 +67,40 @@ void grfc::grafcet::write_as_json_tofile(std::string name) {
 	outfile.close();
 }
 
-//Clears Console
-void CLS()
-{
-#if defined _WIN32
-	system("cls");
-#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
-	system("clear");
-#endif
-}
-
 grfc::grafcet cli_interface_get_full_grafcet() {
 
 #if defined _USER_TESTING_ 
-	int node_count = 0;
-	std::cout << "Anzahl der Nodes: ";
-	std::cin >> node_count;
+
+	int node_count = grfc::user_input::node::get_node_count();
 
 	//NODES
 	std::vector<grfc::node> ALL_NODES;
 
 	for (int node_i = 0; node_i < node_count; node_i++) {
 		//INITIAL STEP
-		CLS();
+		grfc::user_input::CLS();
 		std::cout << "Aktuelle Node: " << node_i << std::endl;
-		bool _is_node_initial = false;
-		std::cout << "Ist die Node der Initial Schritt ( 1:Ja | 0:Nein ): ";
-		std::cin >> _is_node_initial;
-		std::cout << std::endl;
+		bool _is_node_initial = grfc::user_input::node::get_is_node_initial();
 
 		//STEP NUMBER
 		int node_step = node_i;
 
 		//TRANSITION
-		CLS();
+		grfc::user_input::CLS();
 		std::cout << "Aktuelle Node: " << node_i << std::endl;
-		bool is_transition_inverted = false;
-		std::cout << "Soll die Transition Invertiert sein ( 1:Ja | 0:Nein ): ";
-		std::cin >> is_transition_inverted;
+		bool is_transition_inverted = grfc::user_input::transition::get_is_transition_initial();
 
-		int transition_statement_count = 0;
-		std::cout << "Anzahl der Bedingungen in der Transition: ";
-		std::cin >> transition_statement_count;
+		int transition_statement_count = grfc::user_input::transition::get_transition_statement_count();
 
 		std::vector<grfc::single_statement> transition_single_statements_vector;
 
 		for (int transition_statement_i = 0; transition_statement_i < transition_statement_count; transition_statement_i++) {
-			bool is_single_statement_inverted = false;
-			std::cout << "Ist das Statement Invertiert ( 1:Ja | 0:Nein ):";
-			std::cin >> is_single_statement_inverted;
+			bool is_single_statement_inverted = grfc::user_input::single_statement::get_is_single_statement_inverted();
 
-			std::string single_statement_identifier_input;
-			std::cout << "Geben Sie EINEN Bezeichner ein (BuchstabeZahl) Beispiel(x1,S2,P3): ";
-			std::cin >> single_statement_identifier_input;
+			std::string single_statement_identifier_input = grfc::user_input::single_statement::get_single_statement_identifier_input();
 
 			if (transition_statement_i + 1 < transition_statement_count) {
-				int single_statement_conn_input = 0;
-				std::cout << "Welche Verbindung besteht zum naechsten Bezeichner (1:Und-Verknuepfung | 2:Oder-Verknuepfung): ";
-				std::cin >> single_statement_conn_input;
+				int single_statement_conn_input = grfc::user_input::single_statement::get_single_statement_conn_input();
 
 				grfc::conn single_statement_conn = single_statement_conn_input == 1 ? grfc::conn::dot : grfc::conn::plus;
 
@@ -139,48 +111,42 @@ grfc::grafcet cli_interface_get_full_grafcet() {
 		}
 
 		grfc::transition NODE_TRANSITION(transition_single_statements_vector, is_transition_inverted);
+		
 
 		//EXPRESSION
-		CLS();
+		grfc::user_input::CLS();
 		std::cout << "Aktuelle Node: " << node_i << std::endl;
 		std::vector<grfc::single_statement> node_expression_vector;
 
-		int expression_block_count = 0;
-		std::cout << "Anzahl der Aktionen der Node: ";
-		std::cin >> expression_block_count;
+		int expression_block_count = grfc::user_input::expression::get_expression_block_count();
 
 		for (int expression_count_i = 0; expression_count_i < expression_block_count; expression_count_i++) {
 
-			std::string single_statement_identifier_input;
-			std::cout << "Geben Sie EINEN Bezeichner ein (BuchstabeZahl) Beispiel(x1,S2,P3): ";
-			std::cin >> single_statement_identifier_input;
+			std::string single_statement_identifier_input = grfc::user_input::single_statement::get_single_statement_identifier_input();
 
-			int single_statement_action_input = 0;
-			std::cout << "Geben Sie EINE Aktion ein ( 0:Setzen | 1:Nicht-setzen ): ";
-			std::cin >> single_statement_action_input;
+			int single_statement_action_input = grfc::user_input::single_statement::get_single_statement_action_input();
+
 			node_expression_vector.push_back(grfc::single_statement(false, grfc::identifier{ single_statement_identifier_input[0], single_statement_identifier_input.back() - '0' }, (grfc::action)single_statement_action_input));
 		}
 
 		grfc::expression NODE_EXPRESSION(node_expression_vector);
 
 		//CONNECTED TO INITIAL
-		CLS();
+		grfc::user_input::CLS();
 		std::cout << "Aktuelle Node: " << node_i << std::endl;
 		std::pair<bool, int> NODE_CONNECTED_TO_INTITIAL;
 
-		std::cout << "Ist der Schritt mit dem Initial Schritt verbunden sein ( 1:Ja | 0:Nein ): ";
-		std::cin >> NODE_CONNECTED_TO_INTITIAL.first;
+		NODE_CONNECTED_TO_INTITIAL.first = grfc::user_input::expression::get_node_connected_to_initial_first();
 
 		if (NODE_CONNECTED_TO_INTITIAL.first) {
-			std::cout << "Welche Nummer hat der Schritt: ";
-			std::cin >> NODE_CONNECTED_TO_INTITIAL.second;
+			NODE_CONNECTED_TO_INTITIAL.second = grfc::user_input::expression::get_node_connected_to_initial_second();
 		}
 		else {
 			NODE_CONNECTED_TO_INTITIAL.second = 0;
 		}
 
 		//NODE
-		CLS();
+		grfc::user_input::CLS();
 		ALL_NODES.push_back(grfc::node(_is_node_initial, node_step, NODE_TRANSITION, NODE_EXPRESSION, NODE_CONNECTED_TO_INTITIAL));
 	}
 
@@ -189,25 +155,16 @@ grfc::grafcet cli_interface_get_full_grafcet() {
 #elif defined _AUTO_DEBUGING_
 
 	std::vector<grfc::single_statement> trans_statement = {
-	grfc::single_statement(false, grfc::identifier{'S',1},grfc::conn::dot), grfc::single_statement(false,grfc::identifier{'S',2},grfc::conn::plus), grfc::single_statement(true,grfc::identifier{'S',3},grfc::conn::non) };
+	grfc::single_statement(grfc::grafcet_timer(3.0,2,grfc::conn::non),nullptr,false), grfc::single_statement(false,grfc::identifier{'S',2},grfc::conn::plus,nullptr,false), grfc::single_statement(true,grfc::identifier{'S',3},grfc::conn::non,nullptr,false) };
 
-	std::vector<grfc::grafcet_timer> trans_timer = {
-		grfc::grafcet_timer(2.0,1,grfc::conn::plus),
-		grfc::grafcet_timer(3.0,2,grfc::conn::non)
-	};
-
-	grfc::transition transition_test(trans_statement, trans_timer);
+	grfc::transition transition_test(trans_statement,false);
 
 	std::vector<grfc::single_statement> expr{
-		grfc::single_statement(false,grfc::identifier{ 'x', 1 }, grfc::action::unset),
-		grfc::single_statement(false,grfc::identifier{ 'x', 2 }, grfc::action::unset)
+		grfc::single_statement(false,grfc::identifier{ 'x', 1 }, grfc::action::unset,nullptr,false),
+		grfc::single_statement(grfc::grafcet_timer(2.0,4),nullptr,false)
 	};
 
-	std::vector<grfc::grafcet_timer> timer{
-		grfc::grafcet_timer(5.0,3),
-		grfc::grafcet_timer(2.0,4)
-	};
-	grfc::expression c(expr, timer);
+	grfc::expression c(expr);
 
 	auto ALL_NODES = std::vector<grfc::node>{ grfc::node(true, 1, transition_test, c),grfc::node(false, 2, transition_test, c),grfc::node(false, 3, transition_test, c),grfc::node(false, 4, transition_test, c) };
 
